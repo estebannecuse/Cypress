f5518d42407427c9d1839ad440c0e2e9eb2a2a8f
Esteban Necuse <estebannecuse@gmail.com>
2025-04-20 18:42:30 -0300

    "download-upload flow"

---
+++ Primer Proyecto/cypress/integration/examples/uploadDownload.js
@@ -0,0 +1,12 @@
+describe('upload-download test', ()=>{
+    it("verify excel upload download",()=>{
+
+        const FilePath = Cypress.config("fileServerFolder")+"/cypress/Primer Proyecto/cypress/downloads/download.xlsx"
+        cy.visit("https://rahulshettyacademy.com/upload-download-test/index.html")
+        cy.get("#downloadButton").click();
+        cy.task(writeExcelTetst,{searchText: "Apple",replaceText:350,change:{rowChange:0,colChange:2},filePath:FilePath})
+        cy.get("#fileinput").selectFile(FilePath)
+        //importante sobre la linea 8, el boton para el upload tiene que tener type+file, no hay workaround, sino el "selectFile" de cypress no funciona
+        
+    })
+})
